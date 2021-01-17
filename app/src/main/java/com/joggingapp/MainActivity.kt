a832c1d469f7fe4ce9ed947c80b6ee8aa9400a46
ivVane <iv.vane@gmail.com>
2021-01-17 16:28:49 +0100

    Set up Dagger Hilt for Dependency Injection into our App.

--- app/src/main/java/com/joggingapp/MainActivity.kt
+++ app/src/main/java/com/joggingapp/MainActivity.kt
@@ -2,10 +2,23 @@ package com.joggingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.joggingapp.db.RunDAO
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    // This is for testing purpose only to see if our RunDAO works.
+    @Inject
+    lateinit var runDAO: RunDAO
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // This is for testing purpose only will be deleted
+        Log.d("runDao", "RUNDAO: ${runDAO.hashCode()}" )
     }
 }
