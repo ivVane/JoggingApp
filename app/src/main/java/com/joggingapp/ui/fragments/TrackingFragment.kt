31c24282c629e891d96c7a18fe7e4f73ef2cc26f
ivVane <iv.vane@gmail.com>
2021-01-22 01:33:29 +0100

    Handling Map Lifecycle.

--- app/src/main/java/com/joggingapp/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/joggingapp/ui/fragments/TrackingFragment.kt
@@ -1,13 +1,62 @@
 package com.joggingapp.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.google.android.gms.maps.GoogleMap
 import com.joggingapp.R
 import com.joggingapp.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_tracking.*
 
 @AndroidEntryPoint
 class TrackingFragment : Fragment(R.layout.fragment_tracking) {
 
     private val viewModel: MainViewModel by viewModels()
+
+    private var map: GoogleMap? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mapView.onCreate(savedInstanceState)
+
+        mapView.getMapAsync {
+            map = it
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView?.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView?.onPause()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    // We are calling the map Asynchronisly (mapView.getMapAsync), and the app needs to load the
+    // map every time we open our device. The function under (onSaveInstanceState()) can helping us
+    // to cache the map so the app don't need to loaded every time we open our device.
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView?.onSaveInstanceState(outState)
+    }
 }
