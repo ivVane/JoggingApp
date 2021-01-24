5da98307b4eeb8ce9a081cc48b123e436d5bc4f9
ivVane <iv.vane@gmail.com>
2021-01-24 22:35:05 +0100

    Canceling a Run

--- app/src/main/java/com/joggingapp/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/joggingapp/ui/fragments/TrackingFragment.kt
@@ -2,16 +2,19 @@ package com.joggingapp.ui.fragments
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.PolylineOptions
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.joggingapp.R
 import com.joggingapp.other.Constants.ACTION_PAUSE_SERVICE
 import com.joggingapp.other.Constants.ACTION_START_OR_RESUME_SERVICE
+import com.joggingapp.other.Constants.ACTION_STOP_SERVICE
 import com.joggingapp.other.Constants.MAP_ZOOM
 import com.joggingapp.other.Constants.POLYLINE_COLOR
 import com.joggingapp.other.Constants.POLYLINE_WIDTH
@@ -34,6 +37,17 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
 
     private var currentTimeInMillis = 0L
 
+    private var menu: Menu? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        setHasOptionsMenu(true)
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -47,6 +61,49 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
         }
 
         subscribeToObservers()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.toolbar_tracking_menu, menu)
+        this.menu = menu
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu) {
+        super.onPrepareOptionsMenu(menu)
+        if (currentTimeInMillis > 0L) {
+            this.menu?.getItem(0)?.isVisible = true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.miCancelTracking -> {
+                showCancelTrackingDialog()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showCancelTrackingDialog() {
+        val dialog = MaterialAlertDialogBuilder(requireContext(), R.style.AlertDialogTheme)
+            .setTitle("Cancel the Run?")
+            .setMessage("Are you sure you want to cancel the run and delete all its data?")
+            .setIcon(R.drawable.ic_delete)
+            .setPositiveButton("Yes") { _, _ ->
+                stopRun()
+            }
+            .setNegativeButton("No") { dialogInterface, _ ->
+                dialogInterface.cancel()
+            }
+            .create()
+
+        dialog.show()
+    }
+
+    private fun stopRun() {
+        sendCommandToService(ACTION_STOP_SERVICE)
+        findNavController().navigate(R.id.action_trackingFragment_to_runFragment)
     }
 
     // Function that subscribe to our observers to subscribe to the LiveData objects in our service.
@@ -96,6 +153,7 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
     // stopped or paused state and to Stop it if its currently running.
     private fun toggleRun() {
         if (isTracking) {
+            menu?.getItem(0)?.isVisible = true
             sendCommandToService(ACTION_PAUSE_SERVICE)
         } else {
             sendCommandToService(ACTION_START_OR_RESUME_SERVICE)
@@ -110,6 +168,7 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
             material_button_finish_run.visibility = View.VISIBLE
         } else {
             material_button_toggle_run.text = "Stop"
+            menu?.getItem(0)?.isVisible = true
             material_button_finish_run.visibility = View.GONE
         }
     }
