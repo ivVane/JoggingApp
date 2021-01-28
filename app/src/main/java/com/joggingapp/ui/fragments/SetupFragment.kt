870ab846668a015cdf96269134a76751a53eb868
ivVane <iv.vane@gmail.com>
2021-01-28 02:09:49 +0100

    Saving Name and Weight in SharedPreferences

--- app/src/main/java/com/joggingapp/ui/fragments/SetupFragment.kt
+++ app/src/main/java/com/joggingapp/ui/fragments/SetupFragment.kt
@@ -1,20 +1,70 @@
 package com.joggingapp.ui.fragments
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import com.joggingapp.R
+import com.joggingapp.other.Constants.KEY_FIRST_TIME_TOGGLE
+import com.joggingapp.other.Constants.KEY_NAME
+import com.joggingapp.other.Constants.KEY_WEIGHT
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_setup.*
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class SetupFragment : Fragment(R.layout.fragment_setup) {
+
+    @Inject
+    lateinit var sharedPref: SharedPreferences
+
+    @set:Inject
+    var isFirstAppOpen = true
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        if (!isFirstAppOpen) {
+            val navOptions = NavOptions.Builder()
+                .setPopUpTo(R.id.setupFragment, true)
+                .build()
+            findNavController().navigate(
+                R.id.action_setupFragment_to_runFragment,
+                savedInstanceState,
+                navOptions
+            )
+        }
+
         text_view_continue.setOnClickListener {
-            findNavController().navigate(R.id.action_setupFragment_to_runFragment)
+            val success = writePersonalDataToSharedPref()
+            if (success) {
+                findNavController().navigate(R.id.action_setupFragment_to_runFragment)
+            } else {
+                Snackbar.make(requireView(), "Please enter all the fields", Snackbar.LENGTH_SHORT)
+                    .show()
+            }
         }
     }
 
+    private fun writePersonalDataToSharedPref(): Boolean {
+        val name = text_input_edit_text_name.text.toString()
+        val weight = text_input_edit_text_weight.text.toString()
+        if (name.isEmpty() || weight.isEmpty()) {
+            return false
+        }
+
+        sharedPref.edit()
+            .putString(KEY_NAME, name)
+            .putFloat(KEY_WEIGHT, weight.toFloat())
+            .putBoolean(KEY_FIRST_TIME_TOGGLE, false)
+            .apply()
+
+        val toolbarText = "Let's go, $name!"
+        requireActivity().material_text_view_toolbar_title.text = toolbarText
+        return true
+    }
 }
